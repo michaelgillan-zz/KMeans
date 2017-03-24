f26a2099795bc6a50f4341072fef0d54d3d7021e
mjg15 <mjg15@ic.ac.uk>
2017-03-24 17:37:30 +0000

    k is now passed in as argument to program

--- src/main.java
+++ src/main.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class main {
     public static void main(String[] args) {
         Relation relation = ARFFImporter.importARFF(args[0]);
-        List<List<Vector>> partitions = KMeans.runKMeans(3, relation.data);
+        List<List<Vector>> partitions = KMeans.runKMeans(Integer.parseInt(args[1]), relation.data);
         ScatterPlot sp = new ScatterPlot(partitions, relation.relation);
         sp.plot();
     }
